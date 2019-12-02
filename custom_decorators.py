84e809740ba9f50484f3e53fec19408ef93abb57
Gaura Atreya <allmanpride@gmail.com>
2019-12-02 20:42:46 +0545

    Updated Send mail
    * checks for history of worklog for the same day.
    * added the variables for debugging purpose
    * updated the console with aesthetic colors for warnings and logs.

---
+++ custom_decorators.py
@@ -0,0 +1,72 @@
+from functools import wraps
+
+
+# 0=reset
+# 1=bold
+# 4=underline
+# 30-37=normal fg colors
+# 40-47=normal bg colors
+# 90-97=bright fg colors
+# 100-107=bright bg colors
+
+
+def color_console(color):
+    def funct(func):
+        @wraps(func)
+        def print_in_color(*args, **kwargs):
+            print(get_color_string(color))
+            try:
+                func(*args, **kwargs)
+            finally:
+                print(get_color_string())
+
+        return print_in_color
+    return funct
+
+
+def get_color_string(color='reset'):
+    color_dict={
+        'red':"\u001b[31m",
+        'bred':"\u001b[91m",
+        'green':"\u001b[32m",
+        'yellow':"\u001b[33m",
+        'magenta':"\u001b[35m",
+        'reset':"\u001b[0m"
+    }
+    return color_dict[color]
+
+def get_bgcolor_string(color='reset'):
+    color_dict={
+        'red':"\u001b[41m",
+        'green':"\u001b[42m",
+        'yellow':"\u001b[43m",
+        'magenta':"\u001b[45m",
+        'reset':"\u001b[0m"
+    }
+    return color_dict[color]
+
+def get_text_style_string(style='reset'):
+    color_dict={
+        'bold':"\u001b[1m",
+        'underline':"\u001b[4m",
+        'reversed':"\u001b[7m",
+        'reset':"\u001b[0m"
+    }
+    return color_dict[style]
+
+def cursor_move(direction,n):
+    color_dict={
+            'up': '\u001b[{n}A'.format(n=n),
+            'down': '\u001b[{n}B'.format(n=n),
+            'right': '\u001b[{n}C'.format(n=n),
+            'left': '\u001b[{n}D'.format(n=n)
+    }
+    return color_dict[direction]
+
+def change_color(color='reset'):
+    print(get_color_string(color),end="")
+
+def delete_prev_line():
+    print(cursor_move('up',1),end="")
+    print('\u001b[{n}K'.format(n=2),end="")
+    print(cursor_move('left',1000),end="")
--- send_mail.py
+++ send_mail.py
@@ -6,13 +6,18 @@ import datetime
 from jinja2 import Template
 import json
 
+MAIL_TO_ME=False
+DEBUGGING_MODE=False
+
+from custom_decorators import *
+
 class WorkLog:
     def __init__(self,name='Your Name',email="yourmail@eydean.com"):
         self.name=name
         self.email=email
-        self.mailto='samplemail@eydean.com'
+        self.mailto='samplemail@eydean.com' if MAIL_TO_ME else self.email
         self.cc=None
-        self.date=datetime.date.today()
+        self.date=str(datetime.date.today())
         self.worklog={
         'task_of_today':[],
         'task_completed':[],
@@ -23,11 +28,21 @@ class WorkLog:
         self.html_body=None
         self.rendered_html=None
     
+    def load_history(self):
+        with open("worklog.json","r") as json_file:
+            log=json.load(json_file)[-1]
+        if self.date in log:
+            self.worklog=log[self.date]
+            return True
+        else:
+            return False
+
     def load_html(self):
         with open("mail_template.html") as reader:
             self.html_body=reader.read()
         html_template=Template(self.html_body)
-        self.rendered_html=html_template.render(date=self.date, \
+        self.rendered_html=html_template.render(name=self.name, \
+            date=self.date, \
             task_today=self.worklog['task_of_today'], \
                 task_completed=self.worklog['task_completed'], \
                     issues=self.worklog['problems_faced'], \
@@ -35,29 +50,39 @@ class WorkLog:
                             unsolved_problems=self.worklog['unsolved_problems'], \
                                 plans_tomorrow=self.worklog['planning_for_tomorrow'])
     
+    # @color_console('green')
     def save_worklog(self):
         with open("worklog.json","r") as json_file:
             log=json.load(json_file)
         log.append({str(self.date):self.worklog})
         with open("worklog.json","w") as json_file:
             json.dump(log,json_file)
+        print("Worklog saved to file...")
     
+
     def input_logs(self):
         for key in self.worklog:
+            change_color('green')
             print("Enter: "+key+":<enter twice to end>:")
+            change_color()
             input_string=None
             while (input_string!=''):
                 if input_string:
                     self.worklog[key].append(input_string)
-                input_string=input(">")
+                input_string=input("\t>")
+            delete_prev_line()
+        self.save_worklog()
+        
 
     def send_mail(self):
+        change_color('yellow')
         print("Creating email.... Please wait...")
         MY_KEY=os.getenv('MY_ZOHO_KEY')
         msg = MIMEMultipart()
         msg['From'] = self.email
         msg['To'] = self.mailto
-        msg['Subject'] = "Work Log: "+self.name
+        msg['cc']=self.cc
+        msg['Subject'] = "Re: Work Log: "+self.name
         msg.attach(MIMEText(self.rendered_html, 'html'))
         server = smtplib.SMTP('smtp.zoho.com:587')
         server.starttls()
@@ -66,14 +91,28 @@ class WorkLog:
         print("Sending email.... Please wait...")
         server.sendmail(self.email, self.mailto, text)
         server.quit()
+        change_color('green')
         print("Process completed, your email is sent.")
+        change_color()
 
 if __name__=="__main__":
     try:
         log=WorkLog()
-        log.input_logs()
-        log.save_worklog()
+        print(log.load_history())
+        if log.load_history():
+            change_color('bred')
+            print("You have a worklog entry for this date:")
+            change_color()
+            print("do you want to send that to:"+log.mailto+"?\n<y>/n:")
+            response=input("")
+            if response not in ['\n','y']:
+                log.input_logs()
+        else:
+            log.input_logs()
         log.load_html()
-        log.send_mail()
+        if not DEBUGGING_MODE:
+            log.send_mail()
+        else:
+            print("Debugging mode: reached end of program...")
     except KeyboardInterrupt:
-        print("Process terminated..")
+        print("\nProcess terminated..")
